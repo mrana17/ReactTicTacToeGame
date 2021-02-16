a94229dd1849f4ce8d953030fa04e55950af2216
mrana17 <rm_a@msn.com>
2021-02-16 21:58:55 +0100

    add solution

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
 import styles from "../styles/Board.module.css";
 import Square from "./Square";
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const newSquares = [...squares];
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
+type Props = {
+  squares: string[];
+  onClick(i: number): void;
+};
+export default function Board({ squares, onClick }: Props) {
   function renderSquare(i: number) {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   }
 
   return (
     <div>
-      <div className={styles.status}>{status}</div>
       <div className={styles.boardRow}>
         {renderSquare(0)}
         {renderSquare(1)}
@@ -48,24 +29,4 @@ export default function Board() {
       </div>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
